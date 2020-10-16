d1d61d4bb9d6c2c2d41d7eaefce4b8960fc41a32
brokes6 <brokes6@users.noreply.github.com>
2020-10-16 21:31:59 +0800

    DimPleView控件彻底完善

--- CustomControlLibs/src/main/java/com/example/customcontrollibs/DimPleView.kt
+++ CustomControlLibs/src/main/java/com/example/customcontrollibs/DimPleView.kt
@@ -3,13 +3,14 @@ package com.example.customcontrollibs
 import android.animation.ValueAnimator
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.*
-import android.os.Build
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.PathMeasure
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.LinearInterpolator
 import androidx.annotation.ColorInt
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import java.lang.Math.acos
 import java.lang.Math.cos
@@ -37,12 +38,14 @@ class DimPleView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
     private var particleNumber = 1000//粒子数量
     private var particleRadius = 2.2f//粒子半径
     private var diffusionRadius = 268f//扩散圆半径
+    private var radius = 100;//粒子扩散长度
 
     init {
         val array: TypedArray? = context?.obtainStyledAttributes(attrs, R.styleable.DimPleView)
         array?.getInteger(R.styleable.DimPleView_particlesNum, 1000)?.let { setParticleNumber(it) }
         array?.getFloat(R.styleable.DimPleView_particlesRadius, 2.2f)?.let { setParticleRadius(it) }
-        array?.getFloat(R.styleable.DimPleView_particlesDiffusionRadius, 268f)?.let { setDiffusionRadius(it) }
+        array?.getFloat(R.styleable.DimPleView_particlesDiffusionRadius, 350f)?.let { setDiffusionRadius(it) }
+        array?.getInt(R.styleable.DimPleView_strokeOffSet, 100)?.let { setStrokeOffSet(it) }
         array?.getInt(R.styleable.DimPleView_strokeColor, R.color.While)?.let { setStrokeColor(it) }
         animator.duration = 2000
         animator.repeatCount = -1
@@ -51,24 +54,43 @@ class DimPleView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
             updateParticle(it.animatedValue as Float)
             invalidate()
         }
-        paint.color =  ContextCompat.getColor(context!!,R.color.colorAccent)
+        paint.color = ContextCompat.getColor(context!!, R.color.colorAccent)
         paint.isAntiAlias = true
     }
 
+    /*
+    设置粒子数量
+     */
     private fun setParticleNumber(value: Int) {
         this.particleNumber = value;
     }
 
+    /*
+    设置粒子半径半径
+     */
     private fun setParticleRadius(value: Float) {
         this.particleRadius = value;
     }
 
+    /*
+    设置扩散圆半径
+     */
     private fun setDiffusionRadius(value: Float) {
         this.diffusionRadius = value;
     }
 
+    /*
+    设置粒子颜色
+     */
     fun setStrokeColor(@ColorInt value: Int) {
         paint.color = value
+    }
+
+    /*
+    设置粒子扩散长度
+     */
+    fun setStrokeOffSet(value: Int) {
+        radius = value
     }
 
 
@@ -77,7 +99,7 @@ class DimPleView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
             if (particle.offSet > particle.maxOffSet) {
                 particle.offSet = 0f
                 particle.speed = random.nextInt(3) + 1.5f
-                particle.maxOffSet = random.nextInt(250).toFloat()
+                particle.maxOffSet = random.nextInt(radius).toFloat()
             }
             particle.x = (mWidth / 2 + cos(particle.angle) * (diffusionRadius + particle.offSet)).toFloat() + particle.offSetX * particle.direction
 
@@ -116,14 +138,14 @@ class DimPleView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
         pathMeasure.setPath(path, false)
         for (i in 0..particleNumber) {
             pathMeasure.getPosTan(i / particleNumber.toFloat() * pathMeasure.length, pos, tan)
-            val offSet = random.nextInt(200)
+            val offSet = random.nextInt(radius)
             val speed = random.nextInt(2) + 0.5f
             val randomX = random.nextInt(6) - 3f
             val randomY = random.nextInt(6) - 3f
             val offSetX = random.nextInt(3)
             val direction = random.nextInt(3) - 1.5f
             val angel = acos(((pos[0] - mWidth / 2) / diffusionRadius).toDouble())
-            val maxOffSet = random.nextInt(250) + 0f
+            val maxOffSet = random.nextInt(radius) + 0f
             particleList.add(
                     Particle(
                             pos[0] + randomX,
